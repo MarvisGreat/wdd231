c1e695bb09ce05ab3b786436395f3e9815d31ee9
MarvisGreat <nmarvellous@byupathway.edu>
2026-09-07 00:44:55 +0100

    progress

---
+++ scrIpts/homepage-date.js
@@ -0,0 +1,7 @@
+const currentYear = document.querySelector('#currentyear')
+const lastModified = document.querySelector('#lastModified')
+
+const today = new Date();
+
+year.innerHTML = `getFullYear(): <span class="highlight">${today.getFullYear()}</span>`;
+document.getElementById("lastModified").innerHTML = document.lastModified;
